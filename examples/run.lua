4221e85a2327f341be4eefa502709f5af42c487f
Jannik Buhr <jannik.m.buhr@gmail.com>
2022-06-09 14:31:15 +0200

    great progress on #3
    the test lua file can now create a hidden buffer with all the
    non-language lines set to spaces and the lsp client automatically
    attaches to the buffer!

--- examples/run.lua
+++ examples/run.lua
@@ -1,7 +1,6 @@
 local quarto = R'quarto'
 local q = vim.treesitter.query
 local api = vim.api
-local bufnr = 14
 
 
 local function lines(str)
@@ -44,58 +43,66 @@ local function get_language_content(bufnr, language)
   end
 
   return results
-
 end
 
+local function spaces(n)
+  local s = {}
+  for i=1,n do
+    s[i] = ' '
+  end
+  return s
+end
 
-local language = 'python'
-local text = get_language_content(bufnr, language)
-
-
-
--- TODO: it might be easier to get all the lines
--- not belonging to the language, copying the buffer
--- and replace those lines with spaces
-local function get_non_language_lines(bufnr, language)
-  -- get and parse AST
-  local language_tree = vim.treesitter.get_parser(bufnr, 'markdown')
-  local syntax_tree = language_tree:parse()
-  local root = syntax_tree[1]:root()
-
-  -- create capture
-  local query = vim.treesitter.parse_query('markdown',
-  string.gsub([[
-  (fenced_code_block
-    (info_string
-      (language) @lang
-      (#eq? @lang $language)
-    )
-    (code_fence_content) @code (#offset! @code)
-  )
-  ]], "%$(%w+)", {language=language})
-  )
-
-  -- get text ranges
-  local results = {}
-  for _, _, metadata in query:iter_matches(root, bufnr) do
-    -- line numbers start at 0
-    -- {start line, col, end line, col}
-    table.insert(results,metadata.content[1])
+local function create_language_buffer(qmd_bufnr, language)
+  local language_lines = get_language_content(qmd_bufnr, language)
+  local nmax = api.nvim_buf_line_count(qmd_bufnr)
+  local qmd_path = api.nvim_buf_get_name(qmd_bufnr)
+  local postfix
+  if language == 'python' then
+    postfix = '.py'
+  elseif language == 'r' then
+    postfix = '.R'
   end
 
-  return results
+  -- create buffer filled with spaces
+  local bufname_lang = qmd_path..postfix
+  local bufuri_lang = 'file://'..bufname_lang
+  local bufnr_lang = vim.uri_to_bufnr(bufuri_lang)
+  api.nvim_buf_set_name(bufnr_lang, bufname_lang)
 
+  -- local bufnr_lang = api.nvim_create_buf(false, false)
+  -- api.nvim_buf_set_name(bufnr_lang, bufname_lang)
+  api.nvim_buf_set_lines(bufnr_lang, 0, nmax, false, spaces(nmax))
+
+  -- write langue lines
+  for _,t in ipairs(language_lines) do
+    api.nvim_buf_set_lines(bufnr_lang, t.range[1], t.range[3], false, t.text)
+  end
+  return bufnr_lang
 end
 
-lines = get_non_language_lines(bufnr, language)
-P(lines)
 
--- -- create new emtpy buffer
--- local buf = api.nvim_create_buf(true, false)
--- -- api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
---
--- for _,t in ipairs(text) do
---   -- does not write empty lines
---   api.nvim_buf_set_lines(buf, t.range[1], t.range[3], false, t.text)
--- end
+
+-- open ./index.qmd
+-- get bufnr of qmd document for testing
+-- with :ls
+local qmd = 1
+local lang = 'python'
+local bufnr_py = create_language_buffer(qmd, lang)
+P(bufnr_py)
+-- show buffer to debug
+-- local cmd = 'vsplit | b'..bufnr_py
+-- vim.cmd(cmd)
+
+
+
+vim.lsp.for_each_buffer_client(bufnr_py, function(client, client_id, bufnr)
+  P(client_id)
+end)
+
+
+-- delete debug buffer
+-- api.nvim_buf_delete(bufnr_py, {force=true})
+
+
 
